3e960d850c6437e56d0c5b29acc817d22a256196
lauren_m <laurenamolloy@gmail.com>
2025-11-19 11:48:27 +0200

    Added conditional rendering for shoecard

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,18 @@
 import { arrowRight } from '../assets/icons'
-import Button from '../components/Button'
-import { statistics } from '../constants/index'
-import { bigShoe1 } from '../assets/images'
+import Button from '../components/Button';
+import { useState } from 'react';
+import { shoes, statistics } from '../constants/index';
+import { bigShoe1 } from '../assets/images';
+import ShoeCard from '../components/ShoeCard';
 
 const Hero = () => {
+  const [bigShoeImg, setbigShoeImg] = useState(bigShoe1)
   return (
     <section
     id="home"
-    className= "w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 gap-10 max-container border-red-500 p-2">
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: padding-x pt-28">
+    className= "relative w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 gap-10 max-container border-red-500 p-2">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: 
+      padding-x pt-28">
         <p className="text-xl font-montserrat">Our Summer Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max:smleading=[82px]">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
@@ -27,13 +31,24 @@ const Hero = () => {
         })}
         </div>
       </div>
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero">
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img 
-        src={bigShoe1} 
+        src={bigShoeImg} 
         alt="shoe" 
         width={610} 
         height={500}
         className="object-contain relative z-10"></img>
+      </div>
+      <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+        {shoes.map((shoe, idx) => (
+          <div key={idx}>
+            <ShoeCard
+            imgURL={shoe}
+            changeBigShoeImg = {(shoe) => setbigShoeImg(shoe)}
+            bigShoeImg = {bigShoeImg} 
+            />
+          </div>
+        ))}
       </div>
     </section>
   )
